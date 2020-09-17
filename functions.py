8e1b0b5ac589a1277cc9934da7c48e5e824fa783
Moises Baltazar <me@moisesb.com>
2020-09-17 10:16:17 -0500

    Update variables defined in split_subject function

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import os
 
+
 def split_subject(_1):
     SUBJECT = _1
     components = SUBJECT.split(":")
@@ -11,19 +12,21 @@ def split_subject(_1):
 
     proj, subject_id, classifier, extra = components
     return {
-        "PROJECT": proj,
+        "SUBJECT_PROJECT": proj,
         "SUBJECT_ID": subject_id,
         "SUBJECT_CLASSIFIER": classifier,
         "SUBJECT_EXTRA": extra,
-        "SESSION": f"{subject_id}_{classifier}",
+        "SUBJECT_SESSION": f"{subject_id}_{classifier}",
     }
 
 
 def choose_node(_2):
     pass
 
+
 def chain_jobs_on_pbs():
     pass
+
 
 def make_directories(WORKING_DIR):
     os.makedirs(WORKING_DIR, exist_ok=True)
