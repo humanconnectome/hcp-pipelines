9db7aee599d229295503e0ca374e094c77d99dc3
Moises Baltazar <me@moisesb.com>
2020-10-05 19:29:51 -0500

    Add latest version of files

--- util.py
+++ util.py
@@ -1,6 +1,26 @@
+import subprocess
+
+
 def escape_path(text):
     return text.replace("\\", "\\\\").replace("/", "\\/").replace("&", "\\&")
 
 
 def keep_resting_state_scans(scans):
     return [x for x in scans if not (x.startswith("t") or x.startswith("f"))]
+
+
+def shell_run(cmd):
+    return subprocess.check_output(cmd, shell=True, text=True).strip()
+
+
+def qsub(cmd, prior_job=None, afterok=True):
+    if cmd is None:
+        return prior_job
+
+    if prior_job is None:
+        full_cmd = f"qsub {cmd}"
+    else:
+        afterok = "afterok" if afterok else "afterany"
+        full_cmd = f"qsub -W depend={afterok}:{prior_job} {cmd} "
+    jobnumb = shell_run(full_cmd)
+    return jobnumb
