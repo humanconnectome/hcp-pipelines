d6e843d55344e3c7b9ea4855ea02f948d6e38173
Moises Baltazar <me@moisesb.com>
2020-11-09 16:17:23 -0600

    Refactor sessionid.py to be more functional

--- lib/WorkingDirPut/catalog/ToolsHCP/resources/scripts/sessionid.py
+++ lib/WorkingDirPut/catalog/ToolsHCP/resources/scripts/sessionid.py
@@ -1,39 +1,34 @@
 import requests
 import argparse
-import sys
-import json
-import inspect
 
-def get_json_response_list_of_lists(username, password, request_url, keys):
-    response_list = list()
-
-    response = requests.get(request_url, auth=(username, password))
-    if (response.status_code != 200):
-        print(inspect.stack()[0][3] + ": Cannot get response from request: " + request_url)
-        sys.exit(1)
-
-    if not 'application/json' in response.headers['content-type']:
-        print(inspect.stack()[0][3] + ": Unexpected response content-type: " + response.headers['content-type'] + " from " + request_url)
-        sys.exit(1)
-
-    json_response = json.loads(response.text)
-    json_result_set = json_response['ResultSet']
-    json_record_count = int(json_result_set['totalRecords'])
-    json_result = json_result_set['Result']
-    xrange=range
-    for i in xrange(0, json_record_count):
-        item_list = list()
-        for key in keys:
-            item_list.append(str(json_result[i][key]))
-        response_list.append(item_list)
-
-    return response_list
+def generate_url(server, project, subject):
+    request_url = "http://" + server + "/data/projects/" + project + "/subjects/" + subject + "/experiments"
+    return request_url
     
 
-def main():
+def get_sessions(username, password, request_url):
+    response = requests.get(request_url, auth=(username, password))
+    if (response.status_code != 200) or 'application/json' not in response.headers['content-type']:
+        raise Exception("Server response is not OK.", request_url, response.content)
 
+    json_result = response.json()['ResultSet']['Result']
+    return json_result
+
+def find_session_id(server, subject, project, username, password, target_session):
+    request_url = generate_url(server, project, subject)
+    sessions = get_sessions(username, password, request_url)
+
+    if type(sessions) is not list:
+        raise Exception( "I thought sessions was a list, turns out it's probably a dict. I can't test because server is down. Please append .values() to sessions")
+
+    for item in sessions:
+        session, sessionid = item['label'], str(item['ID'])
+        if target_session == session:
+            return str(sessionid)
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script to get session id")
-
     parser.add_argument("-s", "--server", dest="server", default="db.humanconnectome.org", type=str,
                         help="database server system, defaults to db.humanconnectome.org")
     parser.add_argument("-u", "--username", dest="username", required=True, type=str)
@@ -42,16 +37,7 @@ def main():
                         help="project in which to find session")
     parser.add_argument("-su", "--subject", dest="subject", required=True, type=str)
     parser.add_argument("-se", "--session", dest="session", required=True, type=str)
-
     args = parser.parse_args()
 
-    session_and_session_id_list = list()
-    request_url = "http://" + args.server + "/data/projects/" + args.project + "/subjects/" + args.subject + "/experiments"
-    session_and_session_id_list = get_json_response_list_of_lists(args.username, args.password, request_url, ['label', 'ID'])
-    
-    for session_and_session_id in session_and_session_id_list:
-        if args.session == session_and_session_id[0]:
-            print('%s' % session_and_session_id[1])
-
-if __name__ == '__main__':
-    main()
+    sessionid = find_session_id(args.server, args.subject, args.project, args.username, args.password, args.session)
+    print(sessionid)
