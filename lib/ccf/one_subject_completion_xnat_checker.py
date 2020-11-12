0ee7433831ab4ff88d35983f581f826b06eed061
Moises Baltazar <me@moisesb.com>
2020-11-12 16:39:03 -0600

    Replace Checker subclasses since they can be replaced with variables

--- lib/ccf/one_subject_completion_xnat_checker.py
+++ lib/ccf/one_subject_completion_xnat_checker.py
@@ -10,44 +10,49 @@ import sys
 from utils import os_utils, file_utils
 import ccf.archive as ccf_archive
 
-hcp_run_utils = os_utils.getenv_required("HCP_RUN_UTILS")
-xnat_pbs_jobs = os_utils.getenv_required("XNAT_PBS_JOBS")
 
-
-class OneSubjectCompletionXnatChecker(abc.ABC):
-    """
-    Abstract base class for classes that are used to check the completion
-    of pipeline processing for one subject
-    """
-
-    def __init__(self, project, subject, classifier, scan):
-        self.SUBJECT_PROJECT = project
-        self.SUBJECT_ID = subject
-        self.SUBJECT_CLASSIFIER = classifier
+class OneSubjectCompletionXnatChecker:
+    def __init__(
+        self,
+        project,
+        subject,
+        classifier,
+        scan,
+        OUTPUT_RESOURCE_NAME,
+        PIPELINE_NAME,
+        HCP_RUN_UTILS,
+        XNAT_PBS_JOBS,
+    ):
+        self.HCP_RUN_UTILS = HCP_RUN_UTILS
+        self.XNAT_PBS_JOBS = XNAT_PBS_JOBS
         self.SUBJECT_EXTRA = scan
         self.SUBJECT_SESSION = f"{subject}_{classifier}"
+        self.OUTPUT_RESOURCE_NAME = OUTPUT_RESOURCE_NAME
+        self.PIPELINE_NAME = PIPELINE_NAME
         self.archive = ccf_archive.CcfArchive(project, subject, classifier, scan)
 
-    @property
-    def processing_name(self):
-        """Name of processing type to check (e.g. StructuralPreprocessing, FunctionalPreprocessing, etc.)"""
-        raise NotImplementedError
-
     def prereq_dirs(self):
-        raise NotImplementedError
+        if self.PIPELINE_NAME == "StructuralPreprocessing":
+            return self.archive.structural_unproc()
+        else:
+            return self.archive.structural_preproc()
 
     def my_resource(self):
-        raise NotImplementedError
+        archive = self.archive
+        return archive.subject_resources + "/" + self.OUTPUT_RESOURCE_NAME
 
-    def list_of_expected_files(self, working_dir, fieldmap):
+    def list_of_expected_files(self, working_dir):
+        if self.PIPELINE_NAME == "DiffusionPreprocessing":
+            fieldmap = "NONE"
+        else:
+            fieldmap = "SpinEcho"
+
         # define all possible locations
-        common = (
-            f"{self.processing_name}/ExpectedOutputFiles-FieldMap-{fieldmap}.CCF.txt"
-        )
+        common = f"{self.PIPELINE_NAME}/ExpectedOutputFiles-FieldMap-{fieldmap}.CCF.txt"
         alts = [
             "/pipeline_tools/pipelines/expected_files/DiffusionPreprocessing.txt",
-            f"{hcp_run_utils}/{common}",
-            f"{xnat_pbs_jobs}/{common}",
+            f"{self.HCP_RUN_UTILS}/{common}",
+            f"{self.XNAT_PBS_JOBS}/{common}",
         ]
 
         # look in all locations and find first file that exists
@@ -70,7 +75,12 @@ class OneSubjectCompletionXnatChecker(abc.ABC):
             )
             return l
 
-    def is_processing_complete(self, fieldmap, output=sys.stdout):
+    def is_processing_complete(self, output=None):
+        if output is None:
+            output = sys.stdout
+        else:
+            output = open(output, "w")
+
         resource = self.my_resource()
 
         # Check if it exists
@@ -86,7 +96,7 @@ class OneSubjectCompletionXnatChecker(abc.ABC):
         if resource_timestamp > max_prereq_timestamp:
             # resource is newer than all the prerequisite resources
             # check to see if all the expected files exist
-            expected_file_list = self.list_of_expected_files(resource, fieldmap)
+            expected_file_list = self.list_of_expected_files(resource)
             return do_all_files_exist(expected_file_list, output)
         else:
             print(
@@ -105,88 +115,6 @@ def do_all_files_exist(file_name_list, output=sys.stdout):
             all_files_exist = False
 
     return all_files_exist
-
-
-class StructuralCompletionChecker(OneSubjectCompletionXnatChecker):
-    @property
-    def processing_name(self):
-        return "StructuralPreprocessing"
-
-    def my_resource(self):
-        archive = self.archive
-        return archive.subject_resources + "/Structural_preproc"
-
-    def prereq_dirs(self):
-        return self.archive.structural_unproc()
-
-
-class StructuralHandEditCompletionChecker(OneSubjectCompletionXnatChecker):
-    @property
-    def processing_name(self):
-        return "StructuralPreprocessingHandEdit"
-
-    def my_resource(self):
-        archive = self.archive
-        return archive.subject_resources + "/Structural_preproc_handedit"
-
-    def prereq_dirs(self):
-        return self.archive.structural_preproc()
-
-
-class FunctionalCompletionChecker(OneSubjectCompletionXnatChecker):
-    @property
-    def processing_name(self):
-        return "FunctionalPreprocessing"
-
-    def my_resource(self):
-        archive = self.archive
-        return archive.subject_resources + "/" + archive.SUBJECT_EXTRA + "_preproc"
-
-    def prereq_dirs(self):
-        archive = self.archive
-        return [(archive.subject_resources + "/Structural_preproc")]
-
-
-class MultirunicafixCompletionChecker(OneSubjectCompletionXnatChecker):
-    @property
-    def processing_name(self):
-        return "MultiRunIcaFixProcessing"
-
-    def my_resource(self):
-        archive = self.archive
-        return archive.subject_resources + "/MultiRunIcaFix_proc"
-
-    def prereq_dirs(self):
-        archive = self.archive
-        return [(archive.subject_resources + "/Structural_preproc")]
-
-
-class MsmAllCompletionChecker(OneSubjectCompletionXnatChecker):
-    @property
-    def processing_name(self):
-        return "MsmAllProcessing"
-
-    def my_resource(self):
-        archive = self.archive
-        return archive.subject_resources + "/MsmAll_proc"
-
-    def prereq_dirs(self):
-        archive = self.archive
-        return [(archive.subject_resources + "/Structural_preproc")]
-
-
-class DiffusionCompletionChecker(OneSubjectCompletionXnatChecker):
-    @property
-    def processing_name(self):
-        return "DiffusionPreprocessing"
-
-    def my_resource(self):
-        archive = self.archive
-        return archive.subject_resources + "/Diffusion_preproc"
-
-    def prereq_dirs(self):
-        archive = self.archive
-        return [(archive.subject_resources + "/Structural_preproc")]
 
 
 if __name__ == "__main__":
@@ -230,29 +158,14 @@ if __name__ == "__main__":
     subject = args.subject
     classifier = args.classifier
     scan = args.scan
-    # if PIPELINE_NAME == "DiffusionPreprocessing":
-    if False:
-        fieldmap = "NONE"
-    else:
-        fieldmap = "SpinEcho"
-    completion_checker = FunctionalCompletionChecker(
+    completion_checker = OneSubjectCompletionXnatChecker(
         project,
         subject,
         classifier,
         scan,
     )
 
-    if args.output:
-        processing_output = open(
-            args.output,
-            "w",
-        )
-    else:
-        processing_output = sys.stdout
-
-    if completion_checker.is_processing_complete(
-        fieldmap=fieldmap, verbose=True, output=processing_output, short_circuit=False
-    ):
+    if completion_checker.is_processing_complete(args.output):
         print("Exiting with 0 code - Completion Check Successful")
         exit(0)
     else:
