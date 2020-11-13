b3f91fa1328b0f074a1e830471b13cc4df2f5bb8
Moises Baltazar <me@moisesb.com>
2020-11-13 09:35:38 -0600

    Add a python based XNAT_GET script to replace bash

---
+++ templates/generic/XNAT_GET.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+from ccf.get_cinab_style_data import PipelinePrereqDownloader
+from .shared_values import (
+    project,
+    subject,
+    classifier,
+    extra,
+    PIPELINE_NAME,
+    XNAT_PBS_JOBS_ARCHIVE_ROOT,
+    session,
+    WORKING_DIR,
+)
+
+tmp_dir = f"{WORKING_DIR}/tmp"
+os.makedirs(tmp_dir, exist_ok=True)
+os.makedirs(f"{WORKING_DIR}/{session}", exist_ok=True)
+
+prereq = PipelinePrereqDownloader(
+    project,
+    subject,
+    classifier,
+    extra,
+    copy=False,
+    log=False,
+    output_dir=tmp_dir,
+    XNAT_PBS_JOBS_ARCHIVE_ROOT=XNAT_PBS_JOBS_ARCHIVE_ROOT,
+)
+prereq.get_data_for_pipeline(PIPELINE_NAME, remove_non_subdirs=True)
+
+# {% block post %}
+print("Moving files")
+os.system(
+    f'find  {tmp_dir}/{session}/* -maxdepth 0 -type d ! -name "ProcessingInfo" -exec mv {"{}"} {WORKING_DIR}/{session} \;'
+)
+
+print("Removing tmp dir")
+os.system(f"rm -r {tmp_dir}")
+# {% endblock post %}
