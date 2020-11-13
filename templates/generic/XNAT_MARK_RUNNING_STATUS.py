ceb0da6619a48d04bb643bd0ac102567d3854351
Moises Baltazar <me@moisesb.com>
2020-11-12 18:19:05 -0600

    Transfer shared values to separate python file

--- templates/generic/XNAT_MARK_RUNNING_STATUS.py
+++ templates/generic/XNAT_MARK_RUNNING_STATUS.py
@@ -2,24 +2,16 @@
 import argparse
 import os
 import subprocess
-from xnat_file_client import XnatFileClient
+from .shared_values import client, g_scan
 
 
-serverlist = "{{ XNAT_PBS_JOBS_PUT_SERVER_LIST }}"
-server = "{{ PUT_SERVER }}"
-project = "{{ SUBJECT_PROJECT }}"
-subject = "{{ SUBJECT_ID }}"
-session = "{{ SUBJECT_SESSION }}"
-credentials_file = "{{ XNAT_CREDENTIALS_FILE }}"
-g_resource="RunningStatus"
-g_scan="{% if PIPELINE_NAME == 'FunctionalPreprocessing' %}_{{ SUBJECT_EXTRA }}{% endif %}"
+g_resource = "RunningStatus"
 
-client = XnatFileClient(project, subject, session, serverlist, credentials_file)
 
-directory=f"{{XNAT_PBS_JOBS_BUILD_DIR}}/{{SUBJECT_PROJECT}}/{{ PIPELINE_NAME }}.{{SUBJECT_ID}}_{{SUBJECT_CLASSIFIER}}{g_scan}_RUNNING_STATUS"
-file=f"{{ PIPELINE_NAME }}.{{SUBJECT_ID}}_{{SUBJECT_CLASSIFIER}}{g_scan}.RUNNING"
-existing_file=f"{{XNAT_PBS_JOBS_ARCHIVE_ROOT}}/{{ SUBJECT_PROJECT }}/arc001/{{SUBJECT_SESSION}}/RESOURCES/RunningStatus/{file}"
-path=f"{directory}/{file}"
+directory = f"{{XNAT_PBS_JOBS_BUILD_DIR}}/{{SUBJECT_PROJECT}}/{{ PIPELINE_NAME }}.{{SUBJECT_ID}}_{{SUBJECT_CLASSIFIER}}{g_scan}_RUNNING_STATUS"
+file = f"{{ PIPELINE_NAME }}.{{SUBJECT_ID}}_{{SUBJECT_CLASSIFIER}}{g_scan}.RUNNING"
+existing_file = f"{{XNAT_PBS_JOBS_ARCHIVE_ROOT}}/{{ SUBJECT_PROJECT }}/arc001/{{SUBJECT_SESSION}}/RESOURCES/RunningStatus/{file}"
+path = f"{directory}/{file}"
 
 parser = argparse.ArgumentParser("Set up a running status file")
 parser.add_argument("--status", choices=["queued", "done"], default="done")
