66cd4ba95777d7add2e7c21b841262a9d1aab322
Mike Hodge <hodgem@wustl.edu>
2020-11-14 09:41:54 -0600

    Point to FSL603 container for non-diffusion jobs.  Set processing step to CHECK_DATA

--- util.py
+++ util.py
@@ -11,6 +11,9 @@ def keep_resting_state_scans(scans):
 
 def shell_run(cmd):
     return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
+    #result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    #print(result.returncode, result.stdout, result.stderr)
+    #return None;
 
 
 def qsub(cmd, prior_job=None, afterok=True):
