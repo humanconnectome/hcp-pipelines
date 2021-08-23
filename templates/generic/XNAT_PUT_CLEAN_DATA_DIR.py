05ef1cbe53864c544924d21734dd9d9b5e1f8164
Moises Baltazar <me@moisesb.com>
2021-08-23 17:16:56 -0500

    Introduce `DELETE_DATA_AFTER_PUT` flag
    Simple flag which determines whether the working dir and the clean dir
    will be deleted after the clean dir is PUT to xnat.

--- templates/generic/XNAT_PUT_CLEAN_DATA_DIR.py
+++ templates/generic/XNAT_PUT_CLEAN_DATA_DIR.py
@@ -26,8 +26,10 @@ subprocess.call(["chmod", "--recursive", "a+r", str(CLEAN_DATA_DIR)])
 print("Putting new data into DB.")
 client.upload_resource_filepath(resource, str(CLEAN_DATA_DIR), reason, use_http=False)
 
+#{% if DELETE_DATA_AFTER_PUT %}
 print("Removing working_dir: ", str(WORKING_DIR))
 subprocess.call(["rm", "-rf", str(WORKING_DIR)])
 
 print("Removing clean_data_dir: ", str(CLEAN_DATA_DIR))
 subprocess.call(["rm", "-rf", str(CLEAN_DATA_DIR)])
+#{% endif %}
